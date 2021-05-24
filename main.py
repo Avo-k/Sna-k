8643e289c8cbe21fba6a700296b2e185c2ae56d3
Avo-k <jeanlelong.23@gmail.com>
2021-05-24 17:10:28 +0200

    trying things

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pygame
 from pygame import K_UP, K_DOWN, K_RIGHT, K_LEFT
 import random
 
-WIDTH = 400
-HEIGHT = 400
-square = 50
+WIDTH = 500
+HEIGHT = 500
+SQUARE = 50
 FPS = 30
 
 # define colors
@@ -26,31 +26,28 @@ clock = pygame.time.Clock()
 class Snake(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((square, square))
+        self.image = pygame.Surface((SQUARE, SQUARE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
-        self.rect.topleft = (square*3, square*3)
+        self.rect.topleft = (SQUARE * 3, SQUARE * 3)
 
         self.speed = 150 / FPS  # speed is relative to fps in order to be independent
         self.next_dir = None
         self.dir = None  # doesnt move at the beginning of the game
 
-        self.new = True
+        self.new = 0
         first_tail = Bodypart(self)
         all_sprites.add(first_tail)
         self.body = [first_tail]
 
     def update(self):
-        if all(x % square == 0 for x in self.rect.topleft) and \
+        """direct the snake using keyboard inputs"""
+        if all(x % SQUARE == 0 for x in self.rect.topleft) and \
                 {self.dir, self.next_dir} not in [{K_UP, K_DOWN}, {K_RIGHT, K_LEFT}]:
 
-            if self.new:
-                for b in reversed(self.body):
-                    b.dir = b.father.dir
-            else:
-                for b in reversed(self.body[:-1]):
-                    b.dir = b.father.dir
-                self.new = True
+            for b in list(reversed(self.body))[self.new:]:
+                b.dir = b.parent.dir
+            self.new = 0
 
             self.dir = self.next_dir
 
@@ -68,42 +65,42 @@ class Snake(pygame.sprite.Sprite):
         new_tail = Bodypart(self.body[-1])
         self.body.append(new_tail)
         all_sprites.add(new_tail)
-        self.new = False
+        self.new = 1
 
 
 class Bodypart(pygame.sprite.Sprite):
     def __init__(self, parent):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((square, square))
+        self.image = pygame.Surface((SQUARE, SQUARE))
         self.image.fill(GREEN)
         self.rect = self.image.get_rect()
         self.rect.center = parent.rect.center
         self.dir = None
-        self.father = parent
+        self.parent = parent
 
 
 class Apple(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('pic/Applepix.png')
-        self.image = pygame.transform.scale(self.image, (square, square))
+        self.image = pygame.transform.scale(self.image, (SQUARE, SQUARE))
         self.rect = self.image.get_rect()
-        self.rect.topleft = random.sample(range(0, WIDTH + 1 - square, square), 2)
+        self.rect.topleft = random.sample(range(0, WIDTH + 1 - SQUARE, SQUARE), 2)
 
         self.eaten = False
 
     def update(self):
         if self.eaten:
-            self.rect.topleft = random.sample(range(0, WIDTH + 1 - square, square), 2)
+            self.rect.topleft = random.sample(range(0, WIDTH + 1 - SQUARE, SQUARE), 2)
             self.eaten = False
 
 
 def draw_screen(s):
     """fill the screen and draw the grid"""
     s.fill(BLACK)
-    for row in range(1, 8):
-        pygame.draw.line(s, WHITE, (row * 50, 0), (row * 50, 400))
-        pygame.draw.line(s, WHITE, (0, row * 50), (400, row * 50))
+    for row in range(1, 11):
+        pygame.draw.line(s, WHITE, (row * SQUARE, 0), (row * SQUARE, HEIGHT))
+        pygame.draw.line(s, WHITE, (0, row * SQUARE), (WIDTH, row * SQUARE))
 
 
 all_sprites = pygame.sprite.Group()
@@ -111,11 +108,11 @@ snake = Snake()
 apple = Apple()
 all_sprites.add(apple)
 all_sprites.add(snake)
-next_dir = None
 
 # Game loop
 running = True
 while running:
+
     # keep loop running at the right speed
     clock.tick(FPS)
 
@@ -133,22 +130,25 @@ while running:
         apple.eaten = True
         snake.eat()
 
+    # did the snake hit itself ?
     if snake.rect.collidelist([bp.rect for bp in snake.body[1:]]) != -1:
+        print("You tried to eat yourself")
         running = False
-
-    # Update
-    all_sprites.update()
 
     # is the snake still in the screen
     for i in range(2):
-        if 0 > snake.rect.topleft[i] or snake.rect.topleft[i] > WIDTH - snake.rect.width:
+        if 0 > snake.rect.topleft[i] or snake.rect.topleft[i] > WIDTH - SQUARE:
+            print("You went off screen")
             running = False
+
+    # Update
+    all_sprites.update()
 
     # Draw / render
     draw_screen(screen)
     all_sprites.draw(screen)
 
-    # *after* drawing everything, flip the display
+    # flip the display
     pygame.display.flip()
 
 print(f"Score is: {len(snake.body)}")
